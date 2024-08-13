bde826ec4ee0833c7876a944cd5f2c427a27a38a
Alla <urt2509@gmail.com>
2024-08-14 00:42:12 +0200

    task-1 done

--- calculate_salaries/calculate_salary.py
+++ calculate_salaries/calculate_salary.py
@@ -1,4 +1,22 @@
-import pathlib
+import re
 
-def total_salary(path):
-    pass
+def total_salary(path: str) -> tuple[int, float]:
+  
+    with open(path, "r", encoding = "utf-8") as file:
+       total = 0
+       length = 0
+
+       for line in file:
+           pattern = r"\n"
+           replacement = ""
+           modified_line = re.sub(pattern, replacement, line)
+           salary = modified_line.split(',')
+           total += int(salary[1])
+           length +=1
+              
+    average = "{:.2f}".format(total / length)
+    return total, average
+    
+path = "./data.txt"
+total, average = total_salary(path)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
