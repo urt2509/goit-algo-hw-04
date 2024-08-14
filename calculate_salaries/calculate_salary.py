ea3579dceaf6d804c6c57c32ce1546a44f5b970b
Alla <urt2509@gmail.com>
2024-08-15 00:03:13 +0200

    add calculate_salary

---
+++ calculate_salaries/calculate_salary.py
@@ -0,0 +1,32 @@
+import re
+
+
+def total_salary(path: str) -> tuple[int, float]:
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            total = 0
+            length = 0
+
+            for line in file:
+                pattern = r"\n"
+                replacement = ""
+                modified_line = re.sub(pattern, replacement, line)
+                salary = modified_line.split(",")
+                total += int(salary[1])
+                length += 1
+            average = "{:.2f}".format(total / length)
+        if total:
+            return total, average
+        else:
+            print("No salaries are available")
+
+    except FileNotFoundError:
+        print(f"File {path} not found")
+        return None
+
+
+if __name__ == "__main__":
+    path = "./salaries.txt"
+    total, average = total_salary(path)
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
