d82f6e75776dcb6014d9f79567f46327f21eeb5f
Alla <urt2509@gmail.com>
2024-08-14 23:45:38 +0200

    task_1 with exceptions

---
+++ main.py
@@ -0,0 +1,10 @@
+# Task_1
+
+from calculate_salaries.calculate_salary import total_salary
+
+path = "./salaries.txt"
+
+total, average = total_salary(path)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+# Task_2
