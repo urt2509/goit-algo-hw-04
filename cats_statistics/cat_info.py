e9593d5e500f973d2f0a7f4d97ae8ac2452d4093
Alla <urt2509@gmail.com>
2024-08-15 23:06:01 +0200

    refactored task_1, task_2

--- cats_statistics/cat_info.py
+++ cats_statistics/cat_info.py
@@ -1,24 +1,18 @@
-import re
-
-
 def get_cats_info(path: str) -> list[dict]:
     try:
         with open(path, "r", encoding="utf-8") as file:
-            cats_info = list()
 
-            for line in file:
-                pattern = r"\n"
-                replacement = ""
-                modified_line = re.sub(pattern, replacement, line)
-                cat_list = modified_line.split(",")
+            cats_info = [
+                dict(zip(["id", "name", "age"], line.strip().split(",")))
+                for line in file
+            ]
 
-                cat_dictionary = {
-                    "id": cat_list[0],
-                    "name": cat_list[1],
-                    "age": cat_list[2],
-                }
-                cats_info.append(cat_dictionary)
+        return cats_info
 
-            return cats_info
+    except FileNotFoundError:
+        print(f"File {path} not found")
+        return None
+
     except Exception as e:
-        raise e
+        print(f"An unexpected error occurred: {e}")
+        return []
